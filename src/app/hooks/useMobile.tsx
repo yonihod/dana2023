83c4012c1e7891c1550c90ae7891e020c3cbffaa
yoni <yonihod199@gmail.com>
2024-05-26 21:11:37 +0300

    feat: update nobile settings

--- src/app/hooks/useMobile.tsx
+++ src/app/hooks/useMobile.tsx
@@ -1,6 +1,8 @@
 "use client";
 export default function useMobile() {
-  const isMobile = window.matchMedia("(max-width: 640px)").matches;
+  const isMobile =
+    typeof window !== undefined &&
+    window?.matchMedia("(max-width: 640px)").matches;
 
   return { isMobile };
 }
