54225cca5dbe91b099330c4f3ae44591c36a9c75
yoni <yonihod199@gmail.com>
2024-05-26 21:21:15 +0300

    try again

--- src/app/hooks/useMobile.tsx
+++ src/app/hooks/useMobile.tsx
@@ -1,5 +1,8 @@
 "use client";
 export default function useMobile() {
+  if (typeof window === undefined) {
+    return { isMobile: false };
+  }
   const isMobile =
     typeof window !== undefined &&
     window?.matchMedia("(max-width: 640px)").matches;
